958123a91fac2cc0d321958348511e64c44a0505
Dominik Szczepanik <dominik16333@gmail.com>
2023-10-08 16:01:11 +0200

    initial commit

---
+++ src/main/java/com/opensource/queezly/QueezlyApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.queezly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueezlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueezlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensource/queezly/QueezlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.queezly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueezlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
